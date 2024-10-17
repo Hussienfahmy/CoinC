eccf56cf236e907fa440e6a6ab35f1ea6b015ed5
Hussien Fahmy <contact@h-fahmy.com>
2024-10-17 18:14:18 +0300

    removed ios, wasm for now

--- composeApp/src/iosMain/kotlin/com/h_fahmy/coin/MainViewController.kt
+++
@@ -1,5 +0,0 @@
-package com.h_fahmy.coin
-
-import androidx.compose.ui.window.ComposeUIViewController
-
-fun MainViewController() = ComposeUIViewController { App() }
--- composeApp/src/wasmJsMain/kotlin/com/h_fahmy/coin/Platform.wasmJs.kt
+++
@@ -1,7 +0,0 @@
-package com.h_fahmy.coin
-
-class WasmPlatform: Platform {
-    override val name: String = "Web with Kotlin/Wasm"
-}
-
-actual fun getPlatform(): Platform = WasmPlatform()
